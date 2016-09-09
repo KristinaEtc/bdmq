425361fb20e31126562ee70ab1eb38664616a3b0
Kristina <isitiriss@gmail.com>
2016-09-09 17:56:15 +0300

    working server

--- example/server-example.go
+++ example/server-example.go
@@ -1,13 +1,16 @@
 package main
 
 // do not move
-import _ "github.com/KristinaEtc/slflog"
-
 import (
+	"io"
+	"net"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/KristinaEtc/bdmq/tcprec"
 	"github.com/KristinaEtc/config"
+	_ "github.com/KristinaEtc/slflog"
 	"github.com/ventu-io/slf"
 )
 
@@ -48,150 +51,80 @@ var globalOpt = Global{
 
 func main() {
 
-	log.Info("Launching server...")
+	done := make(chan bool)
+	var wg sync.WaitGroup
 
 	// listen on all interfaces
 	config.ReadGlobalConfig(&globalOpt, "server-example")
 	log.Infof("server: %v\n", globalOpt)
-	conns, err := tcprec.Init(globalOpt.Links)
-	if err != nil {
-		log.Error(err.Error())
+
+	wg.Add(len(globalOpt.Links))
+	log.Debugf("num of nodes=%d", len(globalOpt.Links))
+
+	var ln net.Listener
+	var err error
+	for name, link := range globalOpt.Links {
+		log.WithField("id", name).Debug("Running server...")
+
+		// listen on all interfaces
+		ln, err = net.Listen("tcp", link.Address)
+		if err != nil {
+			log.Error(err.Error())
+			continue
+		}
+		go run(&wg, done, link.ID, ln)
 	}
 
-	conn := conns["server1"]
+	wg.Wait()
+}
+
+func run(wg *sync.WaitGroup, done chan (bool), id string, ln net.Listener) {
+	defer func() {
+		wg.Done()
+		done <- true
+	}()
+
+	var conn net.Conn
+	conn, err := ln.Accept()
+	if err != nil {
+		log.WithField("id", id).Error(err.Error())
+		return
+	} else {
+		log.WithField("id", id).Info("accepted")
+	}
 
 	var buf = make([]byte, 256)
 
-	for i := 0; i < 10; i++ {
-
+	for {
 		_, err := conn.Read(buf)
-		if err != nil {
-			log.Error(err.Error())
-			if err == tcprec.ErrMaxRetries {
+		switch err {
+		case io.EOF:
+			log.Debugf("%s detected closed LAN connection", id)
+			conn.Close()
+			conn = nil
+		case tcprec.ErrMaxRetries:
+			log.Warn("sever gave up, reached retry limit")
+			return
+		case nil:
+			log.WithField("id", id).Infof("Msg=%s", string(buf))
+
+			answer := strings.ToUpper(string(buf))
+			_, err := conn.Write([]byte(answer))
+			switch err {
+			case io.EOF:
+				log.Debugf("%s detected closed LAN connection", id)
+				conn.Close()
+				conn = nil
+				return
+			case tcprec.ErrMaxRetries:
 				log.Warn("sever gave up, reached retry limit")
 				return
 			}
+
+		default:
+			log.Debug("default")
+			time.Sleep(time.Second)
+			continue
 		}
-		time.Sleep(time.Second)
-		log.Infof("Msg %s\n", string(buf))
 	}
-
-	// run loop forever (or until ctrl-c)
-	/*for {
-	// will listen for message to process ending in newline (\n)
-	reader := bufio.NewReader(conn)
-	if err != nil {
-		log.Error(err.Error())
-		return
-	}
-
-	_, err = reader.Read(buf)
-	if err != nil {
-		log.Error(err.Error())
-		return
-	}
-
-	//	conn.Read(buf)
-	// output message received
-	log.Infof("Message Received: %s\n", string(buf))
-
-	// sample process for string received
-	//newmessage := strings.ToUpper(string(buf))
-	// send new string back to client
-	//conn.Write([]byte(newmessage + "\n"))
-
-	time.Sleep(time.Second * 1)
-
-	//conn.Write([]byte(strconv.Itoa(i) + "yo\n"))
-	/*writer := bufio.NewWriter(conn)
-	_, err := writer.WriteString(strconv.Itoa(i) + "yo\n")
-	if err != nil {
-		log.Error(err.Error())
-		return
-	}*/
-	//time.Sleep(time.Second)
-	//}
 }
-
-/*
-package main
-
-import (
-	"github.com/KristinaEtc/bdmq/tcprec"
-	"log"
-	"math/rand"
-	"net"
-	"sync"
-	"time"
-)
-
-func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	// open a server socket
-	s, err := net.Listen("tcp", "localhost:7777")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// save the original port
-	addr := s.Addr()
-
-	// connect a client to the server
-	c, err := tcprec.Dial("tcp", s.Addr().String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer c.Close()
-
-	// shut down and boot up the server randomly
-	var swg sync.WaitGroup
-	swg.Add(1)
-	go func() {
-		defer swg.Done()
-		for i := 0; i < 5; i++ {
-			log.Println("server up")
-			time.Sleep(time.Millisecond * 100 * time.Duration(rand.Intn(20)))
-			if err := s.Close(); err != nil {
-				log.Fatal(err)
-			}
-			log.Println("server down")
-			time.Sleep(time.Millisecond * 100 * time.Duration(rand.Intn(20)))
-			s, err = net.Listen("tcp", addr.String())
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}()
-
-	// client writes to the server and reconnects when it has to
-	// this is the interesting part
-	var cwg sync.WaitGroup
-	cwg.Add(1)
-	go func() {
-		defer cwg.Done()
-		for {
-			if _, err := c.Write([]byte("hello, world!\n")); err != nil {
-				switch e := err.(type) {
-				case tcprec.Error:
-					if e == tcprec.ErrMaxRetries {
-						log.Println("client leaving, reached retry limit")
-						return
-					}
-				default:
-					log.Fatal(err)
-				}
-			}
-			log.Println("client says hello!")
-		}
-	}()
-
-	// terminates the server indefinitely
-	swg.Wait()
-	if err := s.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-	// wait for the client to give up
-	cwg.Wait()
-}
-*/
